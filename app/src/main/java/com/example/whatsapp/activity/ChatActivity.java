22f1951d1abcd3c2ba4318218410c09477b59648
Patrick <patrick.silvestre13@hotmail.com>
2021-06-28 07:39:26 -0300

    Salvando mensagem no firebase e preparando recycler view para exibição das mensagens

--- app/src/main/java/com/example/whatsapp/activity/ChatActivity.java
+++ app/src/main/java/com/example/whatsapp/activity/ChatActivity.java
@@ -2,15 +2,29 @@ package com.example.whatsapp.activity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.whatsapp.R;
+import com.example.whatsapp.adapter.MensagensAdapter;
+import com.example.whatsapp.config.ConfiguracaoFirebase;
 import com.example.whatsapp.databinding.ActivityChatBinding;
+import com.example.whatsapp.helper.Base64Custom;
+import com.example.whatsapp.helper.UsuarioFirebase;
+import com.example.whatsapp.model.Mensagem;
 import com.example.whatsapp.model.Usuario;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -19,7 +33,16 @@ public class ChatActivity extends AppCompatActivity {
     private ActivityChatBinding binding;
     private TextView textViewNome;
     private CircleImageView circleImageViewFoto;
+    private EditText editMensagem;
     private Usuario usuarioDestinatario;
+
+    //Identificador usuario remetente e destinatario
+    private String idUsuarioRemetente;
+    private String idUsuarioDestinatario;
+
+    private RecyclerView recyclerMensagens;
+    private MensagensAdapter adapter;
+    private List<Mensagem> mensagens = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +58,11 @@ public class ChatActivity extends AppCompatActivity {
         //Configurações iniciais
         textViewNome = findViewById(R.id.textViewNomeChat);
         circleImageViewFoto = findViewById(R.id.circleImageFotoChat);
+        editMensagem = findViewById(R.id.editMensagem);
+        recyclerMensagens = findViewById(R.id.recyclerMensagens);
+
+        //Recuperar dados do usuario remetente
+        idUsuarioRemetente = UsuarioFirebase.getIdentificadorUsuario();
 
         //Recuperar dados do usuario destinatario
         Bundle bundle = getIntent().getExtras();
@@ -51,7 +79,53 @@ public class ChatActivity extends AppCompatActivity {
             }else {
                 circleImageViewFoto.setImageResource(R.drawable.padrao);
             }
+
+            //Recuperar dados do usuario destinatario
+            idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestinatario.getEmail());
         }
+
+        //Configuração adapter
+        adapter = new MensagensAdapter(mensagens, getApplicationContext());
+
+        //Configuração recyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerMensagens.setLayoutManager(layoutManager);
+        recyclerMensagens.setHasFixedSize(true);
+        recyclerMensagens.setAdapter(adapter);
+
+    }
+
+    public void enviarMensagem(View view){
+        
+        String textoMensagem = editMensagem.getText().toString();
+        
+        if (!textoMensagem.isEmpty()){
+
+            Mensagem mensagem = new Mensagem();
+            mensagem.setIdUsuario(idUsuarioRemetente);
+            mensagem.setMensagem(textoMensagem);
+
+            //Salvar mensagem para remetente
+            salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, mensagem);
+            
+        } else{
+            Toast.makeText(this, "Digite uma mensagem para enviar", Toast.LENGTH_SHORT).show();
+        }
+        
+    }
+
+    private void salvarMensagem(String idRemetente, String idDestinatario, Mensagem msg){
+
+        DatabaseReference database = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference mensagemRef = database.child("mensagens");
+
+        mensagemRef.child(idRemetente)
+                .child(idDestinatario)
+                .push()
+                .setValue(msg);
+
+        //Limpar texto
+        editMensagem.setText("");
 
     }
 
