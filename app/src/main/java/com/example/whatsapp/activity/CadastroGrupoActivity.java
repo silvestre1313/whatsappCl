6818d4aae3205cf7a64db060a3b7e8bb22b7e4e6
Patrick <patrick.silvestre13@hotmail.com>
2021-07-12 19:41:52 -0300

    Salvando imagem do grupo

--- app/src/main/java/com/example/whatsapp/activity/CadastroGrupoActivity.java
+++ app/src/main/java/com/example/whatsapp/activity/CadastroGrupoActivity.java
@@ -1,16 +1,26 @@
 package com.example.whatsapp.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.whatsapp.adapter.GrupoSelecionadoAdapter;
+import com.example.whatsapp.config.ConfiguracaoFirebase;
 import com.example.whatsapp.databinding.ActivityCadastroGrupoBinding;
+import com.example.whatsapp.model.Grupo;
 import com.example.whatsapp.model.Usuario;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -21,9 +31,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.example.whatsapp.R;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CadastroGrupoActivity extends AppCompatActivity {
 
@@ -32,6 +47,10 @@ public class CadastroGrupoActivity extends AppCompatActivity {
     private TextView textTotalParticipantes;
     private GrupoSelecionadoAdapter grupoSelecionadoAdapter;
     private RecyclerView recyclerMembrosSelecionados;
+    private CircleImageView imageGrupo;
+    private static final int SELECAO_GALERIA = 200;
+    private StorageReference storageReference;
+    private Grupo grupo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +66,17 @@ public class CadastroGrupoActivity extends AppCompatActivity {
         //Configurações iniciais
         textTotalParticipantes = findViewById(R.id.textTotalParticipantes);
         recyclerMembrosSelecionados = findViewById(R.id.recyclerMembrosGrupo);
+        imageGrupo = findViewById(R.id.imageGrupo);
+        storageReference = ConfiguracaoFirebase.getFirebaseStorage();
+        grupo = new Grupo();
+
+        //Configurar evento de clique
+        imageGrupo.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            if (intent.resolveActivity(getPackageManager()) != null){
+                startActivityForResult(intent, SELECAO_GALERIA);
+            }
+        });
 
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
@@ -78,4 +108,46 @@ public class CadastroGrupoActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            Bitmap imagem = null;
+
+            try {
+
+                Uri localImagemSelecionada = data.getData();
+                imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemSelecionada);
+
+                if (imagem != null){
+                    imageGrupo.setImageBitmap(imagem);
+
+                    //Recuperar dados da imagem para o firebase
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+                    byte[] dadosImagem = baos.toByteArray();
+
+                    //Salvar imagem no firebase
+                    final StorageReference imagemRef = storageReference
+                            .child("imagens")
+                            .child("grupos")
+                            .child(grupo.getId() +".jpeg");
+
+                    UploadTask uploadTask = imagemRef.putBytes(dadosImagem);
+                    uploadTask.addOnFailureListener(e -> Toast.makeText(CadastroGrupoActivity.this, "Erro ao fazer upload da imagem", Toast.LENGTH_SHORT).show()).addOnSuccessListener(taskSnapshot -> {
+                        Toast.makeText(CadastroGrupoActivity.this, "Sucesso ao fazer upload da imagem", Toast.LENGTH_SHORT).show();
+                        imagemRef.getDownloadUrl().addOnCompleteListener(task -> {
+                            String url = task.getResult().toString();
+                            grupo.setFoto(url);
+                        });
+                    });
+                    
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
